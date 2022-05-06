c493d302e72b18bbd027852d26dbbfd2e57a27dc
fotsoste <steve-didier.fotso-tagne@univ-rouen.fr>
2022-05-06 13:56:18 +0200

    Quatrieme commit

--- src/main/java/fr/univrouen/rss22SB1/Rss22Sb1Application.java
+++ src/main/java/fr/univrouen/rss22SB1/Rss22Sb1Application.java
@@ -1,6 +1,8 @@
 package fr.univrouen.rss22SB1;
 
 import org.springframework.boot.SpringApplication;
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
--- src/main/java/fr/univrouen/rss22SB1/controllers/IndexController.java
+++ src/main/java/fr/univrouen/rss22SB1/controllers/IndexController.java
@@ -1,15 +1,37 @@
 package fr.univrouen.rss22SB1.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class IndexController {
 	
+	
+	
 	@GetMapping("/")
-	public String index() {
-		return "Hello rss22 !";
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView(); 
+		
+		mav.setViewName("index.html");
+		 
+		return mav;
 		
 	}
+	
+	
+	 @Value("${mymessage}")
+    private String message;
+
+	 @GetMapping("/getMessage2")
+	 public ModelAndView getMessage() {
+
+	     var mav = new ModelAndView();
+	     mav.addObject("message", message);
+	     mav.setViewName("show");
+
+	     return mav;
+	 }
 
 }
